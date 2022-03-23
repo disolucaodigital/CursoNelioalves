916d86e35c038e82d2b7509c46ea173ebb6821a9
Sergio Bastos <disolucaodigital@gmail.com>
2022-03-23 08:40:51 -0300

    Criação do Projeto

---
+++ src/main/java/br/com/disolucaodigital/cursonelioalves/CursoNelioAlvesApplication.java
@@ -0,0 +1,13 @@
+package br.com.disolucaodigital.cursonelioalves;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoNelioAlvesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoNelioAlvesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/disolucaodigital/cursonelioalves/CursoNelioAlvesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.disolucaodigital.cursonelioalves;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoNelioAlvesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
