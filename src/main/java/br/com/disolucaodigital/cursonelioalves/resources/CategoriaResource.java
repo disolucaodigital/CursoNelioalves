57b00ce89254b212262d6e1b4d192b738c96717e
Sergio Bastos <disolucaodigital@gmail.com>
2022-03-23 09:47:36 -0300

    Testando a primeira classe de categorias

--- src/main/java/br/com/disolucaodigital/cursonelioalves/resources/CategoriaResource.java
+++ src/main/java/br/com/disolucaodigital/cursonelioalves/resources/CategoriaResource.java
@@ -1,17 +1,28 @@
 package br.com.disolucaodigital.cursonelioalves.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.disolucaodigital.cursonelioalves.domain.Categoria;
 
 @RestController
 @RequestMapping( value = "/categorias")
 public class CategoriaResource {
 	
 	@GetMapping
-	public String listar() {
-		return "REST esta funcionando";
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Escritorio");
+		
+		List<Categoria> listCategorias = new ArrayList<>();
+		listCategorias.add(cat1);
+		listCategorias.add(cat2);
+		return listCategorias;
 	}
 	@GetMapping(value = "/{id}")
 	public String buscaId(@PathVariable Long id) {
