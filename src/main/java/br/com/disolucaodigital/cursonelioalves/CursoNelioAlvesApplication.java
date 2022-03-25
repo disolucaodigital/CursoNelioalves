43c4cee9e816490212c9054dd9b07396f269fa53
Sergio Bastos <disolucaodigital@gmail.com>
2022-03-25 09:59:09 -0300

    Ultimo uso

--- src/main/java/br/com/disolucaodigital/cursonelioalves/CursoNelioAlvesApplication.java
+++ src/main/java/br/com/disolucaodigital/cursonelioalves/CursoNelioAlvesApplication.java
@@ -1,13 +1,32 @@
 package br.com.disolucaodigital.cursonelioalves;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursoNelioAlvesApplication {
+import br.com.disolucaodigital.cursonelioalves.domain.Categoria;
+import br.com.disolucaodigital.cursonelioalves.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursoNelioAlvesApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoNelioAlvesApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria( null, "Informática");
+		Categoria cat2 = new Categoria( null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
